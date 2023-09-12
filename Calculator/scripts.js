81cb1bdb5ed472817365c9896b16e417290d4231
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-09-13 00:32:11 +0600

    operator add

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,6 +1,6 @@
 let display = document.getElementById('display');
 let buttons = document.getElementsByClassName('button');
-console.log(buttons);
+
 let operand1 = 0;
 let operand2 = null;
 let operator = null;
@@ -14,7 +14,39 @@ for(let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click",  () =>{
     let text = buttons[i].textContent;
     if(isOperator(text)){
-      
+      display.textContent = " ";
+      operand2 = 0;
+      return;
     }
+    if(text == '='){
+      if(operator === null){
+        display.textContent = operand1;
+      }
+      else{
+          if(operator == '+'){
+            display.textContent = operand1 + operand2;
+          }
+          if(operator == 'X'){
+            display.textContent = operand1 * operand2;
+          }
+          if(operator == '-'){
+            display.textContent = operand1 - operand2;
+          }
+          if(operator == '/'){
+            display.textContent = operand1 / operand2;
+          }
+      }
+      return;
+    }
+    if(text == 'AC'){
+      operand1 = 0;
+      operand2 = null;
+      operator = null;
+      display.textContent = ' ';
+      return;
+    }
+    display.textContent =  display.textContent + text;
+    display.operand1 = parseFloat(display.textContent)
+    console.log(display.operand1);
   })
 }
